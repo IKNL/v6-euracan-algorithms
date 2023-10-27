1a475bc369b61c568cd6d8865945264efc44fe87
intGRen <hajradhi@hotmail.com>
2023-10-27 15:09:43 +0200

    the error is from the dimnames!

--- chisq/src/R/RPC_statistic.R
+++ chisq/src/R/RPC_statistic.R
@@ -8,3 +8,17 @@ RPC_statistic <- function(data, col, E){
                  dimnames(E)[[2]] %in% dimnames(data)[[2]]]
     return(sum((abs(data - local.E))^2/local.E))
 }
+
+data
+
+test1 <- na.omit(d1[,col])
+test2 <- na.omit(d2[,col])
+test3 <- na.omit(d3[,col])
+
+# dimnames of data are not correct -> order data... -> identify which values belong to cluster i
+E.glob[dimnames(E.glob)[[1]] %in% dimnames(test3)[[1]],]
+
+x <- data.frame(sample(c(1:10), 10, replace = T), row.names = NULL)
+x[,2] <- c(1:nrow(x))
+
+x[which(x[,2] == 4),]
