83807d2029298c3d6f74eec204aa47f019dd993c
intGRen <hajradhi@hotmail.com>
2023-12-19 14:05:58 +0100

    Added one-hot-encoding to coxph and adjusted test.R so that we can test without access to real data.

---
+++ vtg.coxph/src/R/RPC_compute_summed_z.R
@@ -0,0 +1,93 @@
+#' Compute the aggregate statistic of step 2
+#' sum over all distinct times i
+#'   sum the covariates of cases in the set of cases with events at time i.
+#'
+#' Params:
+#'   df: dataframe
+#'   expl_vars: list of explanatory variables (covariates) to use
+#'   time_col: name of the column that contains the event/censor times
+#'   censor_col: name of the colunm that explains whether an event occured or
+#'               the patient was censored
+#'
+#' Return:
+#'   numeric vector with sums and named index with covariates.
+RPC_compute_summed_z <- function(df, subset_rules, expl_vars, time_col,
+                                 censor_col, types = NULL) {
+
+  # Data pre-processing specific to EURACAN
+  df <- vtg.preprocessing::extend_data(df)
+  df <- tryCatch(
+    vtg.preprocessing::subset_data(df, subset_rules),
+    error = function(e) {
+      return(vtg::error_format(conditionMessage(e)))
+    }
+  )
+
+  if (!is.null(df$error)) {
+    vtg::log$error(df$error)
+    return(df)
+  }
+
+  vtg::log$info("Rows before NA removal: {nrow(df)}")
+  df <- na.omit(df[, c(expl_vars, censor_col, time_col)])
+  # TODO: we need to check if sufficient columns are left after NA removal
+  vtg::log$info("Rows after NA removal: {nrow(df)}")
+
+  # Specify data types for the columns in the data
+  if (!is.null(types)) df <- assign_types(df, types)
+
+  data <- preprocess.data(df, expl_vars, censor_col, time_col)
+
+
+  # Set condition to enable univariate Cox
+  if (dim(data$Z)[2] > 1) {
+    cases_with_events <- data$Z[data$censor == 1, ]
+  } else {
+    cases_with_events <- as.matrix(data$Z[data$censor == 1])
+  }
+
+  # We need to check if a factor exists, if it does hot encode before sum.
+  hot.encode.factor <- function(cases_with_events, types){
+      # Because we can have no types mentioned
+      if(is.null(types)){
+          return(cases_with_events)
+      }else{
+          column_names <- names(types)
+
+          # we only want the factor types as this is the option to hot encode
+          all.factors <- lapply(seq(types), function(i){
+              if(types[[i]]$type == "factor"){
+                  return(names(types)[[i]])
+              }
+          })
+          # if no factors, this will be empty, else only return factor names
+          all.factor.names <- Reduce("c", all.factors)
+          # this creates the hot encoding by leveraging R's built in contrast
+          # each factor will have its own independent levels
+          # NOTE : these levels have to be applied before the hot encoding as
+          # the hot encoding looks at the category supplied, if for instance
+          # one data set only has 1:10 and the other 11:20 factor levels,
+          # but the coding is done independently, this will cause errors.
+          # at the central server the types should include the GLOBAL levels.
+          contrast.list <- lapply(seq_along(all.factor.names), function(i) {
+              return(contrasts(cases_with_events[[all.factor.names[i]]],
+                               contrasts = F))
+          })
+          # contrast.arg expects a named list
+          names(contrast.list) = all.factor.names
+          if(!is.null(all.factor.names)){
+              new.formula <- as.formula(paste("~0+", paste(column_names,
+                                                           collapse = "+")))
+              return(model.matrix(new.formula, data = cases_with_events,
+                                  contrast.arg = contrast.list))
+          }else{
+              return(cases_with_events)
+          }
+      }
+  }
+  # Since an item can only be in a single set of events, we're essentially
+  # summing over all cases with events.
+  summed_zs <- colSums(hot.encode.factor(cases_with_events, types))
+
+  return(summed_zs)
+}
---
+++ vtg.coxph/test.R
@@ -0,0 +1,69 @@
+rm(list=ls(all.names = T))
+devtools::load_all("./src")
+devtools::load_all("../vtg.preprocessing")
+
+library(dplyr);library(survival);
+
+
+# Data <- read.table("https://stats.idre.ucla.edu/stat/r/examples/asa/hmohiv.csv"
+                   # , sep=",", header = TRUE)
+
+data1 = read.csv("C://data//euracan-node-a.csv")
+data2 = read.csv("C://data//euracan-node-b.csv")
+datasets = list(data1, data2)
+Data = rbind(data1, data2)
+
+data_local <- rbind(vtg.preprocessing::extend_data(data1),
+                    vtg.preprocessing::extend_data(data2))
+df <- na.omit(df[, c("BMI", "b02_edu", "deadOS", "surv")])
+
+regfit <-  coxph(Surv(surv, deadOS) ~ BMI + b02_edu, data=data_local,
+                 ties="breslow")
+
+# time='surv'
+# event='deadOS'
+
+# path <- "src/data/"
+
+# for (i in dir(path)) load(file = paste0(path,i))
+
+# datasets <- list(vtg.coxph::D1,vtg.coxph::D2,vtg.coxph::D3)
+
+# expl_vars <- c("BMI", "b02_edu")
+# time_col <- c("surv")
+# censor_col <- c("deadOS")
+
+Data <- read.table("https://stats.idre.ucla.edu/stat/r/examples/asa/hmohiv.csv"
+                   , sep=",", header = TRUE)
+Data$area <- gl(floor(nrow(Data)/5), k = 5, length = nrow(Data))
+expl_vars <- c("drug", "age", "area")
+time_col <- c("time")
+censor_col <- c("censor")
+types = list(drug = list(type = "numeric"), age = list(type="numeric"),
+             area = list(type="factor", levels=c(1:20)))
+data1=Data[1:(nrow(Data)/2),]
+data2=Data[((nrow(Data)/2 + 1): nrow(Data)),]
+df=list(data1,data2)
+
+
+data <- data.frame(time=c(4,3,1,1,2,2,3),
+              status=c(1,1,1,0,1,1,0),
+              x=c(0,2,1,1,1,0,0),
+              sex=c(0,0,0,0,1,1,1),
+              censor = c(0,1,0,0,0,0,1))
+expl_vars <- c("x", "sex")
+time_col <- c("time")
+censor_col <- c("censor")
+
+data1 <- data[1:3, ]
+data2 <- data[4:7,]
+df <- list(data1, data2)
+# ties <- "breslow"
+# types <- list(BMI = list(type = "numeric"),
+#               b02_edu = list(type = "factor", levels = c(1,999)),
+#               deadOS = list(type = "factor", levels = c(0,1)))
+
+# First... #
+client <- vtg::MockClient$new(datasets, pkgname = "vtg.coxph")
+fit <- vtg.coxph::dcoxph(client, expl_vars, time_col, censor_col,
+                         types = NULL, organizations_to_include = NULL)
