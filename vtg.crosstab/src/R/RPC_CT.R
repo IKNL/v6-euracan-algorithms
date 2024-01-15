b1380b8bd0df03bacc8c5fd109cc61623c776485
F.C. Martin <f.martin@iknl.nl>
2024-01-15 17:00:06 +0100

    Removed NAs

--- vtg.crosstab/src/R/RPC_CT.R
+++ vtg.crosstab/src/R/RPC_CT.R
@@ -12,25 +12,29 @@
 #'
 #' @export
 #'
-RPC_CT <- function(data, subset_rules, master){
-
-    # Data pre-processing specific to EURACAN
-    df <- vtg.preprocessing::extend_data(data)
-    data <- tryCatch(
-      vtg.preprocessing::subset_data(data, subset_rules),
-      error = function(e) {
-        return(vtg::error_format(conditionMessage(e)))
-      }
-    )
-
-    if (!is.null(data$error)) {
-      vtg::log$error(data$error)
-      return(data)
+RPC_CT <- function(data, subset_rules, master) {
+  # Data pre-processing specific to EURACAN
+  data <- vtg.preprocessing::extend_data(data)
+  data <- tryCatch(
+    vtg.preprocessing::subset_data(data, subset_rules),
+    error = function(e) {
+      return(vtg::error_format(conditionMessage(e)))
     }
+  )
 
-    for (i in all.vars(master$formula)) {
-        data[,i] = factor(data[,i],levels = master$var_cat[[i]])
-    }
-    ct=xtabs(master$formula, data = data)
-    return(ct)
+  if (!is.null(data$error)) {
+    vtg::log$error(data$error)
+    return(data)
+  }
+
+  vtg::log$info("Rows before NA removal: {nrow(data)}")
+  used_variables <- all.vars(master$formula)
+  data <- na.omit(data[, used_variables])
+  vtg::log$info("Rows after NA removal: {nrow(data)}")
+
+  for (i in used_variables) {
+    data[, i] <- factor(data[, i], levels = master$var_cat[[i]])
+  }
+  ct <- xtabs(master$formula, data = data)
+  return(ct)
 }
--- vtg.crosstab/src/R/RPC_get_vars.R
+++ vtg.crosstab/src/R/RPC_get_vars.R
@@ -12,23 +12,27 @@
 #'
 #' @export
 #'
-RPC_get_vars <- function(data, subset_rules, master){
-
-    # Data pre-processing specific to EURACAN
-    df <- vtg.preprocessing::extend_data(data)
-    data <- tryCatch(
-      vtg.preprocessing::subset_data(data, subset_rules),
-      error = function(e) {
-        return(vtg::error_format(conditionMessage(e)))
-      }
-    )
-
-    if (!is.null(data$error)) {
-      vtg::log$error(data$error)
-      return(data)
+RPC_get_vars <- function(data, subset_rules, master) {
+  # Data pre-processing specific to EURACAN
+  data <- vtg.preprocessing::extend_data(data)
+  data <- tryCatch(
+    vtg.preprocessing::subset_data(data, subset_rules),
+    error = function(e) {
+      return(vtg::error_format(conditionMessage(e)))
     }
+  )
 
-    f <- master$formula
-    vars <- apply(data[,all.vars(f)], 2, unique, simplify=F)
-    return(vars)
+  if (!is.null(data$error)) {
+    vtg::log$error(data$error)
+    return(data)
+  }
+
+  vtg::log$info("Rows before NA removal: {nrow(data)}")
+  used_variables <- all.vars(master$formula)
+  data <- na.omit(data[, used_variables])
+  vtg::log$info("Rows after NA removal: {nrow(data)}")
+
+  f <- master$formula
+  vars <- apply(data, 2, unique, simplify = F)
+  return(vars)
 }
